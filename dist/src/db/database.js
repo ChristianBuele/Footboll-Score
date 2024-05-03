a82df4b62f43becc708af7f49df31e481ba6722b
christianbuele <christian97.b@hotmail.es>
2024-05-03 09:46:47 -0500

    update database data

--- dist/src/db/database.js
+++ dist/src/db/database.js
@@ -13,9 +13,9 @@ const sequelize_1 = require("sequelize");
 // );
 const db = new sequelize_1.Sequelize({
     database: 'postgres',
-    username: 'postgres',
+    username: 'postgres.ridzhgpoiefbfjgqkhex',
     password: 'Lucas30.03.199797S',
-    host: 'db.ridzhgpoiefbfjgqkhex.supabase.co',
+    host: 'aws-0-us-east-1.pooler.supabase.com',
     dialect: 'postgres',
     logging: true
 });
--- src/db/database.ts
+++ src/db/database.ts
@@ -13,9 +13,9 @@ import {Sequelize} from 'sequelize';
 const db = new Sequelize(
     {
         database:'postgres',
-        username:'postgres',
+        username:'postgres.ridzhgpoiefbfjgqkhex',
         password:'Lucas30.03.199797S',
-        host:'db.ridzhgpoiefbfjgqkhex.supabase.co',
+        host:'aws-0-us-east-1.pooler.supabase.com',
         dialect:'postgres',
         logging:true
     }
